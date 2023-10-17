3d753050a34da09fa8dddc52091b4da47886e44b
rlopgar <rlopgar599@g.educaand.es>
2023-10-17 18:32:27 +0200

    Tercer Salvado

--- src/Condicionales5.java
+++ src/Condicionales5.java
@@ -16,23 +16,23 @@ public class Condicionales5 {
             int costoCompania=0;
             System.out.println("El costo de cada alumno es "+precioAlumno+" euros, el costo de los autobuses es "+costoCompania+" euros y los pagos totales a la compañia es "+pagoCompania+" euros");
         }
-            else if(numAlumnos>=50 && numAlumnos<=99){
-                int precioAlumno=70;
-                int pagoCompania=numAlumnos*precioAlumno;
-                int costoCompania=0;
-                System.out.println("El costo de cada alumno es "+precioAlumno+" euros, el costo de los autobuses es "+costoCompania+" euros y los pagos totales a la compañia es "+pagoCompania+" euros");
-            }
-                else if(numAlumnos>=30 && numAlumnos<=49){
-                    int precioAlumno=95;
-                    int pagoCompania=numAlumnos*precioAlumno;
-                    int costoCompania=0;
-                    System.out.println("El costo de cada alumno es "+precioAlumno+" euros, el costo de los autobuses es "+costoCompania+" euros y los pagos totales a la compañia es "+pagoCompania+" euros");
-                }
-                    else if(numAlumnos<=29){
-                        int costoCompania=4000;
-                        int precioAlumno=costoCompania/numAlumnos;
-                        int pagoCompania=(numAlumnos*precioAlumno)+costoCompania;
-                        System.out.println("El costo de cada alumno es "+precioAlumno+" euros, el costo de los autobuses es "+costoCompania+" euros y los pagos totales a la compañia es "+pagoCompania+" euros");
-                    }
+        else if(numAlumnos>=50 && numAlumnos<=99){
+            int precioAlumno=70;
+            int pagoCompania=numAlumnos*precioAlumno;
+            int costoCompania=0;
+            System.out.println("El costo de cada alumno es "+precioAlumno+" euros, el costo de los autobuses es "+costoCompania+" euros y los pagos totales a la compañia es "+pagoCompania+" euros");
+        }
+        else if(numAlumnos>=30 && numAlumnos<=49){
+            int precioAlumno=95;
+            int pagoCompania=numAlumnos*precioAlumno;
+            int costoCompania=0;
+            System.out.println("El costo de cada alumno es "+precioAlumno+" euros, el costo de los autobuses es "+costoCompania+" euros y los pagos totales a la compañia es "+pagoCompania+" euros");
+        }
+        else if(numAlumnos<=29){
+            int costoCompania=4000;
+            int precioAlumno=costoCompania/numAlumnos;
+            int pagoCompania=(numAlumnos*precioAlumno);
+            System.out.println("El costo de cada alumno es "+precioAlumno+" euros, el costo de los autobuses es "+costoCompania+" euros y los pagos totales a la compañia es "+pagoCompania+" euros");
+        }
     }
 }
