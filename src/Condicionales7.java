c98b6e6eb8e3ccd2a8add28ba7126abbac6718ff
rlopgar <rlopgar599@g.educaand.es>
2023-10-17 18:41:50 +0200

    Tercer Salvado

--- src/Condicionales7.java
+++ src/Condicionales7.java
@@ -1,2 +1,10 @@
+import java.util.Scanner;
+
 public class Condicionales7 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Introduce el destino%r1-America del norte%r2-America Central");
+    }
 }
